f74ae914370ffd3d2d12307b658cb3208e334065
Elias Guere <eliasguere1025@gmail.com>
2020-07-17 20:38:59 -0500

    Estructura y estilos del carousel, carousel-item y header

---
+++ src/containers/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../assets/styles/app.scss';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+
+const App = () => (
+    <div className="App">
+        <Header />
+        <Search />
+        <Categories>
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+    </div>
+);
+
+export default App;
