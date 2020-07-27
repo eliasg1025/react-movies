c9eca39f2716fdd144f7c766af025e5c8ebcf13a
Elias Guere <eliasguere1025@gmail.com>
2020-07-26 20:33:59 -0500

    Agregando reducers y actions para controlar la lista de los favoritos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-    const initialState = useInitialState(API);
+const Home = ({ mylist, trends, originals }) => {
     return (
         <>
             <Search />
-            {initialState.mylist?.length > 0 &&
+            {mylist?.length > 0 &&
                 <Categories title="Mi lista">
                     <Carousel>
-                        {initialState.mylist?.map(item => (
-                            <CarouselItem key={item.id} {...item}/>
+                        {mylist?.map(item => (
+                            <CarouselItem
+                                key={item.id}
+                                {...item}
+                                isList
+                            />
                         ))}
                     </Carousel>
                 </Categories>
@@ -25,15 +26,18 @@ const Home = () => {
 
             <Categories title="Tendencias">
                 <Carousel>
-                    {initialState.trends?.map(item => (
-                        <CarouselItem key={item.id} {...item} />
+                    {trends?.map(item => (
+                        <CarouselItem
+                            key={item.id}
+                            {...item}
+                        />
                     ))}
                 </Carousel>
             </Categories>
 
             <Categories title="Originales de Platzi Video">
                 <Carousel>
-                    {initialState.originals?.map(item => (
+                    {originals?.map(item => (
                         <CarouselItem key={item.id} {...item} />
                     ))}
                 </Carousel>
@@ -42,4 +46,12 @@ const Home = () => {
     );
 };
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        mylist: state.mylist,
+        trends: state.trends,
+        originals: state.originals
+    };
+};
+
+export default connect(mapStateToProps, null)(Home);
---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+const reducer = ( state, action ) => {
+    switch ( action.type ) {
+        case 'SET_FAVORITE':
+            const exist = state.mylist.find(item => item.id === action.payload.id);
+            if (exist) return {...state}
+
+            return {
+                ...state,
+                mylist: [...state.mylist, action.payload]
+            }
+        case 'DELETE_FAVORITE':
+            return {
+                ...state,
+                mylist: state.mylist.filter(item => item.id !== action.payload)
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
