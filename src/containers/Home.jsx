a4534cda6a2be2bf2a259cf1af3f4af632e329f1
Elias Guere <eliasguere1025@gmail.com>
2020-09-27 20:46:07 -0500

    Completando las validaciones del ui

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,11 +5,13 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Header from '../components/Header';
 
 const Home = ({ mylist, trends, originals }) => {
     return (
         <>
-            <Search />
+            <Header />
+            <Search isHome />
             {mylist?.length > 0 &&
                 <Categories title="Mi lista">
                     <Carousel>
